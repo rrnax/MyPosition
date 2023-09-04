2fed53c6ea88744db24aa639d34cd325373ea92f
rrnax <agent47.rogal@gmail.com>
2023-09-04 15:08:28 +0200

    Updates

---
+++ precache-manifest.47801c0aa6f23d29d35a11793eb84bcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c68ccbb657f44fe9011f",
+    "url": "/MyPosition/static/css/main.4a79d1c0.chunk.css"
+  },
+  {
+    "revision": "c68ccbb657f44fe9011f",
+    "url": "/MyPosition/static/js/main.c68ccbb6.chunk.js"
+  },
+  {
+    "revision": "9b8921e4f19c015d4095",
+    "url": "/MyPosition/static/js/1.9b8921e4.chunk.js"
+  },
+  {
+    "revision": "7fa4432e8635adc98b29",
+    "url": "/MyPosition/static/js/runtime~main.7fa4432e.js"
+  },
+  {
+    "revision": "a9f720b09e38a8fb19fdf24fa0031f5f",
+    "url": "/MyPosition/index.html"
+  }
+];
